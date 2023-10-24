7efb57f3f538e576a2471a4894960e1a7c77bae6
hollyl966 <holly.lee0001@gmail.com>
2023-10-24 12:01:28 -0700

    Update test.R

---
+++ test.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(ggplot2)
+#setwd('C:/Users/holly/Projects/cecs450')
+sleepefficency <- read.csv("./Sleep_Efficiency.csv")
+
+sleepcol <- colnames(sleepefficency)
+sleepcol
+time <- data.frame(sleepefficency)
+time %>% select(c("Age","Bedtime"))
+
+  
+time
+p <- ggplot(time,aes(x="gender",y="Age",fill="Age",color="Age")
+       +geom_bar(stat="identity"))
