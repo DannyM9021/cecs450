94ef2d89586d8bbdb07b5a424b3fb1365a5590d7
DannyM9021 <dmoreno.1109@gmail.com>
2023-10-27 19:41:47 -0700

    Added a way to find the csv file no matter the operating system

--- test.R
+++ test.R
@@ -1,7 +1,12 @@
 library(dplyr)
 library(ggplot2)
-#setwd('C:/Users/holly/Projects/cecs450')
-sleepefficency <- read.csv("./Sleep_Efficiency.csv")
+library(fs)
+
+# Below is to find where the file is located no matter what OS
+# Used https://www.rdocumentation.org/packages/fs/versions/1.6.3/topics/dir_ls
+# as reference
+l <- fs::dir_ls(recurse=TRUE, glob="**/Sleep_Efficiency*csv")
+sleepefficency <- read.csv(l)
 
 sleepcol <- colnames(sleepefficency)
 sleepcol
