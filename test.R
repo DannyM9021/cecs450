0cd399942104cb9597323087636427b6cb7bee8e
helloily100 <Ivy.Ly02@student.csulb.edu>
2023-11-30 11:23:32 -0800

    Update test.R

--- test.R
+++ test.R
@@ -188,7 +188,7 @@ ggplot(alcohol, aes(x = Alcohol.consumption, y = Sleep.efficiency)) +
 smoking_count <- sleep_efficiency_data_frame %>% select(c("Smoking.status"))
 smoking_graph <- barplot(table(smoking_count), main = "Smoking Status", xlab = "Status", ylab = "Count", 
                          border = c("blue", "red"), col = c("lightblue", "lightpink2"), ylim = c(0,320),
-                         args.legend=list(cex=1,x= "topright"), space = 0.1 ) 
+                         legend.text = c("No","Yes"), args.legend=list(cex=1,x= "topright"), space = 0.1 ) 
 
 abline(h=0)
 text(x = smoking_graph,
